21fdf59cf8d9b9616f5aaf0b2f2ad34f0d5dd36b
Douglas Duarte <95700136+dougDuarte@users.noreply.github.com>
2022-01-16 20:08:30 -0300

    Delete color.js

--- color.js
+++
@@ -1,6 +0,0 @@
-const colorPicker = document.querySelector('.settings__style__color')
-const colorArea = document.querySelector('.main__code-container')
-
-colorPicker.addEventListener('input', function() {
-    colorArea.style.backgroundColor = colorPicker.value
-})
